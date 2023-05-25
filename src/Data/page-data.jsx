461603c9e7c14c146782af6734c3bc22ba99ccaf
kamruzzaman sohag <shorkar.sohag74@gmail.com>
2023-05-26 05:54:38 +0600

    first commit

---
+++ src/Data/page-data.jsx
@@ -0,0 +1,67 @@
+export const personalData = {
+	name : "kamruzzaman",
+	role : "Software Engineer",
+	education: [
+		"Bachelor of Science in Computer Science, North South University(2016-2021)."
+		
+	],
+	contactLinks: [
+		"mailto:emailto:shorkar.sohag74@gmail.com",
+		"https://www.facebook.com/kamruzzaman.7474",
+		"https://www.linkedin.com/in/kamruzzaman-sohag-19b49b189/",
+	
+		"https://medium.com/@time2code.ali",
+	],
+};
+
+export const aboutMe = {
+	title: "About Me",
+	body: [
+		"As a highly motivated and driven individual with a passion for making a positive impact, I have a unique blend of technical and interpersonal skills, thanks to my background in computer science. I am able to tackle complex problems with ease, and I never lose sight of my ultimate goal, even when facing challenges.",
+		"When I'm not working, I love hiking, reading, and spending time with my friends and family. I believe that my determination, can-do attitude, and hard work make me a true asset to any team.",
+	],
+};
+
+export const skills = {
+	soft: [
+		{ icon: "👂🏼", text: "Active Listening" },
+		{ icon: "💬", text: "Effective Communication" },
+		{ icon: "👥", text: "Collaboration" },
+		{ icon: "⽓", text: "Teamwork" },
+		{ icon: "💡", text: "Creative Problem Solving" },
+		{ icon: "⌛️", text: "Time management" },
+	],
+	hard: [
+		{ icon: "💻", text: "javascript" },
+		{ icon: "ʦ", text: "TypeScript" },
+		{ icon: "🚀", text: "React" },
+		{ icon: "💾", text:"Nextjs 13" },
+		{ icon: "💾", text: "Mongoose" },
+		{ icon: "📈", text: "Data Structures and Algorithms" },
+	],
+};
+
+export const professionalData = {
+	title: "Professional Experience",
+	experiences: [
+		{
+			role: "Frontend Developer, growMore19. (2021-Present)",
+			description:
+				"i am a passionate and experienced frontend developer. With 1 years of experience in web development, I specialize in creating interactive and user-friendly interfaces for web applications. My primary focus is on frontend technologies such as HTML, CSS, and JavaScript, and I have extensive knowledge of popular frontend frameworks like React, Next js .Throughout my career, I have worked on various projects, ranging from small business websites to large-scale web applications. I have a deep understanding of responsive design principles, ensuring that websites and applications I develop are optimized for different devices and screen sizes.",
+			current: true,
+		},
+
+		{
+			role: " Learning Machine Learning ",
+			description:
+				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
+			current: false,
+		},
+		{
+			role: "Full Stack developer",
+			description:
+				"In addition to their technical abilities, I am a dedicated and proactive team player. They thrive in collaborative environments, value open communication, and are committed to fostering a positive and productive work atmosphere.",
+			current: false,
+		},
+	],
+};
